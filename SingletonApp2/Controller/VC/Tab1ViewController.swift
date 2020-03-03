f58c5b0150c569fb1b62973dd4de05d05b2a6df1
Hiroki Kondo <kondo.matr02@gmail.com>
2020-03-03 19:39:16 +0900

    tab1を編集中です。ビルドがなぜか出来ないので、確認できません・・・。

---
+++ SingletonApp2/Controller/VC/Tab1ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  Tab1ViewController.swift
+//  SingletonApp2
+//
+//  Created by 近藤宏輝 on 2020/03/03.
+//  Copyright © 2020 Fujii Yuta. All rights reserved.
+//
+
+import UIKit
+
+class Tab1ViewController: UITabBarController {
+    
+    
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBOutlet weak var label: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+    
+    @IBAction func startSingleton1(_ sender: Any) {
+        let singleton :Singleton =  Singleton.sharedInstance
+               
+        guard let content = textField.text else { return }
+                
+        singleton.saveContents(content: content)
+        label = singleton.getContents()
+    }
+    
+
+}
